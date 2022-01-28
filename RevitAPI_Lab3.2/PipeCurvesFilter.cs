a63c3b3573b0cec2b8a8d1a238c16a62ae60a288
Tik88 <xtikx@yandex.ru>
2022-01-28 16:50:30 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI_Lab3.2/PipeCurvesFilter.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI.Selection;
+
+namespace RevitAPI_Lab3._2
+{
+    internal class PipeCurvesFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Pipe;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
